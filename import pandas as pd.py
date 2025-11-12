02dbfa8b70b63593333efac66e1fc6aa6774c507
GitHubのユーザー名 <a2508223m@reitaku.jp>
2025-11-12 14:33:38 +0900

    part2

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -3,6 +3,3 @@ import requests
 
 japan_eq = df[df["place"].str.contains("Japan", case=False, na=False)]
 print(f"日本で発生した地震の数：{len(japan_eq)}件")
-
-max_eq = df.loc[df["mag"].idxmax()]
-print(f"最大マグニチュード：{max_eq['mag']}（場所：{max_eq['place']}／日時：{max_eq['time']}）")
